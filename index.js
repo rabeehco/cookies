5de7ccfaad4c1e6acb8dfae8aac93d26ad6a7694
codewithrabeeh <real.ray48@gmail.com>
2022-06-14 13:28:09 +0530

    sign-in cookies

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 const cookieParser = require('cookie-parser')
-app.use(cookieParser())
+app.use(cookieParser('thisismysecret'))
 
 app.get('/greet', (req, res) => {
     const {animal = 'No-name'} = req.cookies
@@ -14,6 +14,17 @@ app.get('/setname', (req, res) => {
     res.cookie('animal', 'dojo duck')
     res.send('Okay, Sent You a Cookie')
 
+})
+
+app.get('/getsignedcookie', (req, res) => {
+    res.cookie('fruit', 'grape', {signed: true})
+    res.send('Ok Signed Your Fruit Cookie')
+})
+
+app.get('/verifyfruit', (req, res) => {
+    console.log(req.cookies) 
+    console.log(req.signedCookies)
+    res.send(req.signedCookies)
 })
 
 app.listen(3000, ()=>{
